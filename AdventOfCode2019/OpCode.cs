05b335b1fafa339e01119308367a4fda3a977ba7
Entroper <entroper@gmail.com>
2019-12-05 05:32:07 -0500

    problems 1 and 2

---
+++ AdventOfCode2019/OpCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    public enum OpCode
+    {
+        Add = 1,
+        Multiply = 2,
+        Halt = 99
+    }
+}
